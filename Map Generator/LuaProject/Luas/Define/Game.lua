35ec54f91fa5506996913815bdb8884c8e1bc07f
Chtho1y <love.code@163.com>
2024-08-14 17:44:14 +0800

    Delete Map Generator/LuaProject directory

--- Map Generator/LuaProject/Luas/Define/Game.lua
+++
@@ -1,6 +0,0 @@
-GameDefine = {}
-
-GameDefine.StreamingAssetsPath = UnityEngine.Application.streamingAssetsPath
-GameDefine.PersistentDataPath = UnityEngine.Application.persistentDataPath
-
-return GameDefine
